9cb211eb2bfb9dcf950da43c500f57fc75712395
miller <hmllsnow@gmail.com>
2024-06-11 22:50:44 +0800

    去掉api，仅仅保留history

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
 model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True, torch_dtype=torch.float16).cuda()
 # model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True,torch_dtype=torch.float16,quantization_config=BitsAndBytesConfig(load_in_4bit=True)).cuda()
 
-def chat(message, history=None):
+def chat(message, history):
     if history is None or len(history) == 0:
         history.append(("你好", "你好, 我是潘金莲, 我将回答你提出来的问题"))
     for response,history in model.stream_chat(tokenizer,message,history,max_length=2048,top_p=0.7,temperature=1):
@@ -23,4 +23,4 @@ gr.ChatInterface(chat,
                 description="""
 我是小莲莲，一个美丽勤劳的女子.  
                  """,
-                 ).queue(1).launch(show_api=True , quiet=True)
+                 ).queue(1).launch()
